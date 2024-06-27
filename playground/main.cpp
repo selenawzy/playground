000cff4fe8b13136c4560477ea6415b4224388e1
Selena Wang <selenawang1999@gmail.com>
2024-06-26 23:06:41 -0400

    Initial Commit

---
+++ playground/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  playground
+//
+//  Created by Selena Wang on 6/26/24.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
